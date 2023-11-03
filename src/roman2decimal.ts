677baf396b929b5ce6ae7fad76939669c4741833
Subha <subharamkie@gmail.com>
2023-11-03 21:53:55 +0000

    Feat:Added corner test cases,error to handle illformed string

--- src/roman2decimal.ts
+++ src/roman2decimal.ts
@@ -1,44 +1,37 @@
 export function convertRomanToDecimal(romanNum: string): number {
+  if (romanNum === "") throw new Error("String is empty");
   const romanLookUpObj: { [key: string]: number } = {
     I: 1,
     V: 5,
-    IX: 9,
     X: 10,
     L: 50,
-    XC: 90,
     C: 100,
     D: 500,
-    CM: 900,
     M: 1000,
   };
   let decimalNumber: number = 0;
   let currentValue: number = 0;
   let nextValue: number = 0;
-  let currentLetter: string = "";
 
   const stringArray = [...romanNum.toUpperCase()];
   for (let index = 0; index < stringArray.length; index++) {
-    currentLetter = stringArray[index];
+    const currentLetter = stringArray[index];
     if (currentLetter in romanLookUpObj) {
-      console.log("decimal to start with:" + decimalNumber);
       currentValue = romanLookUpObj[currentLetter];
-      console.log("value from obj:" + currentValue);
       //keep track of next char to decide for IV,IX etc
       if (stringArray[index + 1]) {
         //if next value if higher than current, move index forward and set value
         nextValue = romanLookUpObj[stringArray[index + 1]];
-        console.log("next value:" + nextValue);
-
         if (nextValue > currentValue) {
           currentValue = nextValue - currentValue;
           decimalNumber += currentValue;
-          console.log("next value greater:" + currentValue);
-          index += 2;
+          index++;
           continue;
         }
       }
       decimalNumber += currentValue;
-      console.log("value from obj:" + decimalNumber);
+    } else {
+      throw new Error("Ill formed string");
     }
   }
 
--- tests/roman2decimal.test.ts
+++ tests/roman2decimal.test.ts
@@ -5,7 +5,35 @@ describe("Convert Roman number to Decimal", () => {
     ["V", 5],
     ["II", 2],
     ["IV", 4],
+    ["VIII", 8],
+    ["IX", 9],
+    ["XII", 12],
+    ["XIV", 14],
+    ["XVIII", 18],
+    ["XIX", 19],
+    ["XXXIX", 39],
+    ["XL", 40],
+    ["LIX", 59],
+    ["LXXII", 72],
+    ["MCMLXXIX", 1979],
+    ["MMIX", 2009],
+    ["MMXVIII", 2018],
+    ["CCCLXVII", 367],
+    ["CMXCIX", 999],
+    ["MCMXCIX", 1999],
   ])("should return Decimal for %i", (x, result) => {
     expect(convertRomanToDecimal(x)).toBe(result);
+  });
+});
+
+describe("Error for empty string", () => {
+  it("Throws error for empty string", () => {
+    expect(() => convertRomanToDecimal("")).toThrow("String is empty");
+  });
+});
+
+describe("Error with wrong letters", () => {
+  it("Throws error for wrong letters", () => {
+    expect(() => convertRomanToDecimal("MCVF")).toThrow("Ill formed string");
   });
 });
