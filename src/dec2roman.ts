9e62a0c021368440e1214fe71827921e5f163ccc
Subha <subharamkie@gmail.com>
2023-11-03 17:44:54 +0000

    Refactor:Cleaned up code,removed console logs

--- src/dec2roman.ts
+++ src/dec2roman.ts
@@ -1,109 +1,85 @@
 import { lookup } from "dns";
-const orderedLookupMap = new Map<number,string>();
-orderedLookupMap.set(1000,'M');
-orderedLookupMap.set(500,'D');
-orderedLookupMap.set(100,'C');
-orderedLookupMap.set(50,'L');
-orderedLookupMap.set(10,'X');
-orderedLookupMap.set(5,'V');
-orderedLookupMap.set(1,'I');
-let appendNine:boolean = false;
-const APPEND_NINE = 'IX';
-    
+const orderedLookupMap = new Map<number, string>();
+orderedLookupMap.set(1000, "M");
+orderedLookupMap.set(500, "D");
+orderedLookupMap.set(100, "C");
+orderedLookupMap.set(50, "L");
+orderedLookupMap.set(10, "X");
+orderedLookupMap.set(5, "V");
+orderedLookupMap.set(1, "I");
+let appendNine: boolean = false;
+const APPEND_NINE = "IX";
 
-export function convertDecToRoman(num:number):string|undefined{
-    let quotient:number = 0;
-    let remainder:number = 0;
-    let currentKeyToGetLetter:number = 0;
-    let closestHigherNum:number = 0;
-    let pad:string|undefined = '';
-    let romanNumeral:string|undefined = ''; 
-    
-    console.log("Num to convert:"+num);
-    if(orderedLookupMap.has(num)){
-        romanNumeral= orderedLookupMap.get(num) ;
-        return romanNumeral;
-    } else { 
-        //last digit not 9
+export function convertDecToRoman(num: number): string | undefined {
+  let quotient: number = 0;
+  let remainder: number = 0;
+  let currentKeyToGetLetter: number = 0;
+  let closestHigherNum: number = 0;
+  let pad: string | undefined = "";
+  let romanNumeral: string | undefined = "";
 
-        if(Math.floor(num%10) === 9) {
-            num = num-9;
-            appendNine = true;
+  if (orderedLookupMap.has(num)) {
+    romanNumeral = orderedLookupMap.get(num);
+    return romanNumeral;
+  } else {
+    //last digit not 9
+    if (Math.floor(num % 10) === 9) {
+      num = num - 9;
+      appendNine = true;
+    }
+    remainder = num;
+    //iterate through the object, divide by each one
+    for (const [key, value] of orderedLookupMap) {
+      //divide number by each key
+      if (remainder >= key) {
+        quotient = Math.floor(remainder / key);
+        remainder = Math.floor(remainder % key);
+        currentKeyToGetLetter = key;
+        console.log("q:" + quotient + ",r:" + remainder);
+        pad = orderedLookupMap.get(closestHigherNum); // should never be undefined
+        if (!pad) {
+          pad = "";
         }
-        remainder = num;
-        //iterate through the object, divide by each one 
-        for(const[key,value] of orderedLookupMap){
-            //divide number by each key
-            if(remainder>=key){
-                quotient = Math.floor(remainder/key);
-                remainder = Math.floor(remainder%key);
-                currentKeyToGetLetter = key;
-                pad = orderedLookupMap.get(closestHigherNum); // should never be undefined
-                if(!pad){pad = '';}
-                romanNumeral += populateRomanString(quotient,remainder,currentKeyToGetLetter,pad);
-            }
-            closestHigherNum = key;//store the closest higher number for numbers like 4,9
-        };
-        if(appendNine){
-            romanNumeral += APPEND_NINE;
-            appendNine = false;
-        }
-        return romanNumeral;
-    }    
-    return '';
+        romanNumeral += populateRomanString(
+          quotient,
+          remainder,
+          currentKeyToGetLetter,
+          pad
+        );
+      }
+      closestHigherNum = key; //store the closest higher number for numbers like 4
+    }
+    if (appendNine) {
+      romanNumeral += APPEND_NINE;
+      appendNine = false;
+    }
+    return romanNumeral;
+  }
+}
+function populateRomanString(
+  num: number,
+  diff: number,
+  keyForMap: number,
+  paddingLetter: string
+): string {
+  let numeral = "";
+  let loopNum = num;
+  if (num > 3) {
+    loopNum = 5 - num;
+    numeral = stringGenerationLoop(loopNum, keyForMap) + paddingLetter;
+  } else {
+    numeral = stringGenerationLoop(loopNum, keyForMap);
+  }
+  return numeral;
+}
+function stringGenerationLoop(times: number, lookupKey: number): string {
+  let returnStr = "";
+  for (let i = 0; i < times; i++) {
+    returnStr += orderedLookupMap.get(lookupKey);
+  }
+  return returnStr;
 }
 
-function populateRomanString(num:number,diff:number,keyForMap:number,paddingLetter:string):string{
-    let numeral = '';
-    let loopNum = num;
-    let noPrefix:boolean = false;
-    
-    if(num>3){
-        loopNum = 5-num;
-        numeral = stringGenerationLoop(loopNum,keyForMap)+paddingLetter;
-    }else{
-        numeral = stringGenerationLoop(loopNum,keyForMap);
-    }
-    /*
-    if(diff < 3){
-        numeral += paddingLetter;
-    }else{
-        numeral = paddingLetter+numeral;
-    }
-    
-    if(num > 3){
-        loopNum = 5-num;
-       // numeral = stringGenerationLoop(loopNum,num)+paddingLetter;
-    }else if(diff > 0 && diff <= 3){
-        loopNum = diff;
-        console.log('loopNum:'+loopNum);
-        noPrefix = true;
-        //numeral = stringGenerationLoop(loopNum,num);
-    }else if(diff > 3){
-        loopNum = 5-diff;
-        prefix = true;
-    }else{
-        loopNum = num;
-        noPrefix = true;
-        //numeral = stringGenerationLoop(loopNum,num);
-    }
-
-    if(prefix){
-        console.log('prefix:'+prefix);
-        numeral = paddingLetter+stringGenerationLoop(loopNum,keyForMap);
-    }else if(noPrefix){
-        console.log('noPrefix:'+noPrefix);
-        numeral = stringGenerationLoop(loopNum,keyForMap);
-    }else {
-        numeral = stringGenerationLoop(loopNum,keyForMap)+paddingLetter;
-    }*/
-    return numeral;
-
-}
-function stringGenerationLoop(times:number,lookupKey:number):string{
-    let returnStr = '';
-    for(let i=0;i<times;i++){
-        returnStr+=orderedLookupMap.get(lookupKey);
-    }
-    return returnStr;
+function multipleOfNine(num: number) {
+  //would be the case for 90,900
 }
--- tests/dec2roman.test.ts
+++ tests/dec2roman.test.ts
@@ -1,16 +1,20 @@
-import {convertDecToRoman} from "../src/dec2roman"
+import { convertDecToRoman } from "../src/dec2roman";
 
-describe("Test convert to roman function",()=>{
-    it.each(
-        [
-            [3,'III'],
-            [4,'IV'],
-            [8,'VIII'],
-            [9,'IX'],
-            [11,'XI'],
-                    ]
-    )("should return Roman numeral for %i", (x,result) =>{
-        expect(convertDecToRoman(x)).toBe(result);
-    });
-    
+describe("Test convert to roman function", () => {
+  it.each([
+    [18, "XVIII"],
+    [19, "XIX"],
+    [20, "XX"],
+    [24, "XXIV"],
+    [25, "XXV"],
+    [34, "XXXIV"],
+    [39, "XXXIX"],
+    [40, "XL"],
+    [44, "XLIV"],
+    [45, "XLV"],
+    [59, "LIX"],
+    [72, "LXXII"],
+  ])("should return Roman numeral for %i", (x, result) => {
+    expect(convertDecToRoman(x)).toBe(result);
+  });
 });
